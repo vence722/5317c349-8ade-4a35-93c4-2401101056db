857564a8e8117afa2e7451ea6c32a7a83281571b
Vence Lam <vence722@gmail.com>
2018-09-16 21:24:19 +0800

    init google maps client

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 go get -u github.com/astaxie/beego
 go get -u github.com/go-sql-driver/mysql
 go get -u gopkg.in/asaskevich/govalidator.v4
+go get -u github.com/satori/go.uuid
+go get -u github.com/vence722/convert
+go get -u googlemaps.github.io/maps
--- services/init.go
+++ services/init.go
@@ -1,15 +1,29 @@
 package services
 
 import (
+	"fmt"
+
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	// mysql driver
 	_ "github.com/go-sql-driver/mysql"
+	"googlemaps.github.io/maps"
 
 	"5317c349-8ade-4a35-93c4-2401101056db/models"
 )
 
+var googleMapsClient *maps.Client
+
 func init() {
+	// init mongo db
+	user := beego.AppConfig.String("mysql_user")
+	pass := beego.AppConfig.String("mysql_pass")
+	db := beego.AppConfig.String("mysql_db")
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:peggy2015@/orders?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8", user, pass, db))
 	orm.RegisterModel(new(models.Order))
+
+	// init google map api
+	apiKey := beego.AppConfig.String("google_map_api_key")
+	googleMapsClient, _ := maps.NewClient(maps.WithAPIKey(apiKey))
 }
