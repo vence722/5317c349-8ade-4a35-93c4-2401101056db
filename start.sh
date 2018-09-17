54e9649d5070ddd423788ecf04e43be1932050e4
Vence Lam <vence722@gmail.com>
2018-09-17 21:32:04 +0800

    finish docker compose

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 docker build -t order-api .
-docker-compose up
+docker-compose up -d
+echo "Start sleep for 60s, in order to wait for MySQL finish starting"
+sleep 60
+echo "Wake up. Now will init the database and the table"
+docker cp ./init_table.sql order-mysql:/init_table.sql
+docker exec order-mysql "mysql -u root -ppassword < /init_table.sql"
+echo "Done."
