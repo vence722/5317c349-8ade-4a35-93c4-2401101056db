99abaaa8d0b88d9aee2466ab1ef533fb265490c7
Vence Lam <vence722@gmail.com>
2018-09-17 22:18:00 +0800

    add info for go get

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-go get -u github.com/astaxie/beego
-go get -u github.com/go-sql-driver/mysql
-go get -u gopkg.in/asaskevich/govalidator.v4
-go get -u github.com/satori/go.uuid
-go get -u github.com/vence722/convert
-go get -u googlemaps.github.io/maps
+go get -u -v github.com/astaxie/beego
+go get -u -v github.com/go-sql-driver/mysql
+go get -u -v gopkg.in/asaskevich/govalidator.v4
+go get -u -v github.com/satori/go.uuid
+go get -u -v github.com/vence722/convert
+go get -u -v googlemaps.github.io/maps
