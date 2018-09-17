989b788cc57dc19972ec968ca33155d6384aaef1
Vence Lam <vence722@gmail.com>
2018-09-17 21:42:21 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "Start sleep for 60s, in order to wait for MySQL finish starting"
 sleep 60
 echo "Wake up. Now will init the database and the table"
 docker cp ./init_table.sql order-mysql:/init_table.sql
-docker exec order-mysql "mysql -u root -ppassword < /init_table.sql"
+docker exec order-mysql sh -c "mysql -u root -ppassword < /init_table.sql"
 echo "Done."
